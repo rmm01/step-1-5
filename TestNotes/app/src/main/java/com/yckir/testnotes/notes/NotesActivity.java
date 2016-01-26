80e4382a7bdfcbeceb4303e0f7c72edbc19c4ab5
Ricky Martinez <crosscountry68@gmail.com>
2016-01-25 18:41:10 -0800

    Initial commit

---
+++ TestNotes/app/src/main/java/com/yckir/testnotes/notes/NotesActivity.java
@@ -0,0 +1,13 @@
+package com.yckir.testnotes.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
